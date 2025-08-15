9784535b7bf6cf7c2babbb96cde32b1cafc40d38
Agostinho Alves <aalvesdeoliveirafilho1@gmail.com>
2025-08-14 21:54:47 -0300

    feat: atv

--- src/app/atv/page.tsx
+++ src/app/atv/page.tsx
@@ -1,9 +1,11 @@
 import MiniDisplay from "@/components/MiniDisplay"
+import Caculadora from "@/components/Calculadora"
 export default function atv_page() {
 
     return (
         <div>
             <MiniDisplay/>
+            <Caculadora/>
         </div>
     )
 }
