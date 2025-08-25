695d6bb57eabc6f744f9b0a1aff13d9e43d4b241
Agostinho Alves <aalvesdeoliveirafilho1@gmail.com>
2025-08-25 18:44:34 -0300

    atv music

--- src/components/LikeresButton.tsx
+++ src/components/LikeresButton.tsx
@@ -1,25 +1,29 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 export default function LikeButton() {
-
   const [IsLiked, setIsLiked] = useState(false);
-  
 
+
+  //o pull request deu problema, mas o codigo é esse aqui e alterei apenas a identação para tentar dar outro pull request
   return (
     <button
-    onClick={()=>{setIsLiked(!IsLiked)}}
+      onClick={() => {
+        setIsLiked(!IsLiked);
+      }}
       className={`flex items-center
         space-x-2 p-2 rounded-b-md
         text-gray-400 hover:text-gray-500 
         bg-gray-50 hover:bg-gray-100 text-7xl
-        ${IsLiked ? "text-red-500 hover:text-red-600 bg-red-100 hover:bg-red-200"  : "text-gray-400 hover:text-gray-500 bg-gray-50 hover:bg-gray-200"}`}
+        ${
+          IsLiked
+            ? "text-red-500 hover:text-red-600 bg-red-100 hover:bg-red-200"
+            : "text-gray-400 hover:text-gray-500 bg-gray-50 hover:bg-gray-200"
+        }`}
     >
-      <span>
-        {IsLiked ? <FaHeart/> : <FaRegHeart/>}
-      </span>
+      <span>{IsLiked ? <FaHeart /> : <FaRegHeart />}</span>
       {IsLiked ? "Curtido" : "Curtir"}
     </button>
   );
